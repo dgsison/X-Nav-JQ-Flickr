69dfa21353f27015489b11810f1868c858f09b5a
diego garcia sison <dsison@zeta03.aulas.gsyc.es>
2015-03-12 20:33:52 +0100

    retoque

---
+++ query.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$("#button").click(function(){
+		var tag = $("#tag").val();	
+
+	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	$.getJSON(flickerAPI, {
+		tags: tag,
+		tagmode: "any",
+		format: "json"
+	})
+		.done(function(data) {
+			$.each(data.items, function(i, item) {
+				$("<img>").attr("src", item.media.m).appendTo("#images");
+				if (i === 3) {
+					return false;
+				}
+			});
+		});
+	});
+});
